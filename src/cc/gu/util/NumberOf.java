d101154adefd322fa450db3c9b7158396c23a2a1
crassorz <crassorz@gmail.com>
2016-07-10 17:38:26 +0800

    rename

--- src/cc/gu/util/NumberOf.java
+++ src/cc/gu/util/NumberOf.java
@@ -1,6 +1,8 @@
 package cc.gu.util;
 
-public class Number {
+import java.util.Objects;
+
+public class NumberOf {
 	final private static String FORMAT_INT = "^([0-9]+)$";
 	final private static String FORMAT_LONG = "(?i)^([0-9]+)l$";
 	final private static String FORMAT_FLOAT = "(?i)^([0-9]+\\.?([0-9])*(e[0-9])?)$";
@@ -12,94 +14,87 @@ public class Number {
 //	final private static String FORMAT_O_0oOp = "(?i)^0O([0-7]+)(\\.[0-7]+)?(p[0-9])$";
 
 	public static boolean isNumber(Object obj) {
-		if (obj == null)
-			return false;
-		else if (obj instanceof java.lang.Number)
-			return true;
-		else if (obj instanceof String)
-			return isNumber(toNumber((String) obj));
-		return false;
+		return toNumber(obj, null) != null;
 	}
 
-	private static Object toNumber(String string) {
-		if (string == null)
-			return null;
-		if (string.matches(FORMAT_INT))
-			return Long.parseLong(string);
-		if (string.matches(FORMAT_LONG))
-			return Long.parseLong(string.substring(0, string.length() - 1));
-		if (string.matches(FORMAT_FLOAT))
-			return Double.parseDouble(string);
-		if (string.matches(FORMAT_X_0xX))
-			return Long.parseLong(string.substring(2, string.length()), 16);
-		if (string.matches(FORMAT_X_Xh))
-			return Long.parseLong(string.substring(0, string.length() - 1), 16);
-		if (string.matches(FORMAT_O_0oO))
-			return Long.parseLong(string.substring(2, string.length()), 8);
-		try {
-			return Double.parseDouble(string);
-		} catch (Exception e) {
-			return null;
+	public static Number toNumber(Object v, Number defaultNumber) {
+		if (v == null)
+			return defaultNumber;
+		else if (v instanceof Number)
+				return (Number) v;
+		else {
+			String string = Objects.toString(v);
+			if (string.matches(FORMAT_INT))
+				return Long.parseLong(string);
+			if (string.matches(FORMAT_LONG))
+				return Long.parseLong(string.substring(0, string.length() - 1));
+			if (string.matches(FORMAT_FLOAT))
+				return Double.parseDouble(string);
+			if (string.matches(FORMAT_X_0xX))
+				return Long.parseLong(string.substring(2, string.length()), 16);
+			if (string.matches(FORMAT_X_Xh))
+				return Long.parseLong(string.substring(0, string.length() - 1), 16);
+			if (string.matches(FORMAT_O_0oO))
+				return Long.parseLong(string.substring(2, string.length()), 8);
+			try {
+				return Double.parseDouble(string);
+			} catch (Exception e) {
+				return defaultNumber;
+			}
 		}
 	}
 
 	public static Byte getNumber(Object v, Byte defaultNumber) {
-		if (v == null)
-			return defaultNumber;
-		else if (v instanceof java.lang.Number)
-			return ((java.lang.Number) v).byteValue();
-		else if (v instanceof String)
-			return getNumber(toNumber((String) v), defaultNumber);
-		return defaultNumber;
+		Number number = toNumber(v, defaultNumber);
+		if (number == null) {
+			return null;
+		} else {
+			return number.byteValue();
+		}
 	}
 
 	public static Short getNumber(Object v, Short defaultNumber) {
-		if (v == null)
-			return defaultNumber;
-		else if (v instanceof java.lang.Number)
-			return ((java.lang.Number) v).shortValue();
-		else if (v instanceof String)
-			return getNumber(toNumber((String) v), defaultNumber);
-		return defaultNumber;
+		Number number = toNumber(v, defaultNumber);
+		if (number == null) {
+			return null;
+		} else {
+			return number.shortValue();
+		}
 	}
 
 	public static Integer getNumber(Object v, Integer defaultNumber) {
-		if (v == null)
-			return defaultNumber;
-		else if (v instanceof java.lang.Number)
-			return ((java.lang.Number) v).intValue();
-		else if (v instanceof String)
-			return getNumber(toNumber((String) v), defaultNumber);
-		return defaultNumber;
+		Number number = toNumber(v, defaultNumber);
+		if (number == null) {
+			return null;
+		} else {
+			return number.intValue();
+		}
 	}
 
 	public static Long getNumber(Object v, Long defaultNumber) {
-		if (v == null)
+		Number number = toNumber(v, defaultNumber);
+		if (number == null) {
 			return null;
-		else if (v instanceof java.lang.Number)
-			return ((java.lang.Number) v).longValue();
-		else if (v instanceof String)
-			return getNumber(toNumber((String) v), defaultNumber);
-		return defaultNumber;
+		} else {
+			return number.longValue();
+		}
 	}
 
 	public static Float getNumber(Object v, Float defaultNumber) {
-		if (v == null)
-			return defaultNumber;
-		else if (v instanceof java.lang.Number)
-			return ((java.lang.Number) v).floatValue();
-		else if (v instanceof String)
-			return getNumber(toNumber((String) v), defaultNumber);
-		return defaultNumber;
+		Number number = toNumber(v, defaultNumber);
+		if (number == null) {
+			return null;
+		} else {
+			return number.floatValue();
+		}
 	}
 
 	public static Double getNumber(Object v, Double defaultNumber) {
-		if (v == null)
-			return defaultNumber;
-		else if (v instanceof java.lang.Number)
-			return ((java.lang.Number) v).doubleValue();
-		else if (v instanceof String)
-			return getNumber(toNumber((String) v), defaultNumber);
-		return defaultNumber;
+		Number number = toNumber(v, defaultNumber);
+		if (number == null) {
+			return null;
+		} else {
+			return number.doubleValue();
+		}
 	}
 }
