3abfb1c443a65b9fa8f03edf1114cdeb7a25424d
crassorz <crassorz@gmail.com>
2016-09-03 02:55:29 +0800

    fix matches bug

--- src/cc/gu/util/debug/ClassLight.java
+++ src/cc/gu/util/debug/ClassLight.java
@@ -6,18 +6,21 @@ public class ClassLight implements Light {
 	
 	final private String klass;
 	public ClassLight(Class klass, boolean inner) {
-		String name = klass.getName().replaceAll(".", "\\.").replaceAll("$", "\\$");
+		String name = klass.getName().replaceAll("\\.", "\\\\.").replaceAll("\\$", "\\\\$");
 		if (inner) {
 			this.klass = String.format("^(%s.*)$", name);
 		} else {
 			this.klass = String.format("^(%s)$", name);
 		}
+		System.out.println(klass);
 	}
 
 	@Override
 	public boolean light(StackTraceElement[] elements, int level, Object self, Throwable e, String format,
 			Object... args) {
-		return elements[0].getClassName().matches(klass);
+		String k = elements[0].getClassName();
+		boolean matches = k.matches(klass);
+		return matches;
 	}
 
 }
